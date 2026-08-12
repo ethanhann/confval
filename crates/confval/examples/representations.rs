92eafb588ac0c49b9867816dbde7f282838ce2fe
Ethan Hann <ethanhann@gmail.com>
2026-08-12 12:38:19 -0400

    Remove move validation as it is now automatically called via the attribute

--- crates/confval/examples/representations.rs
+++ crates/confval/examples/representations.rs
@@ -32,8 +32,7 @@ struct LimitsSpec {
 }
 
 impl Validate for LimitsSpec {
-    fn validate(&self, report: &mut Report) {
-        Mode::keyword_set().check_located(&self.mode, "mode", report);
+    fn validate(&self, _report: &mut Report) {
     }
 }
 
